546adc4f7e5703a6181357feaa2739993ef4ff2f
Guy <guythomas721@gmail.com>
2019-12-03 23:17:04 -0800

    Remove unused styles

--- src/components/Layout/global-styles.js
+++ src/components/Layout/global-styles.js
@@ -8,27 +8,10 @@ const enumerateHeaderStyles = (svgSelector = "", headerPsuedoClass = "") =>
     .join(",")
 
 const headerLinkStyles = css`
-  ${enumerateHeaderStyles()} {
-    position: relative;
-  }
-  ${enumerateHeaderStyles(`.header-anchor`)} {
-    /* Reset Initial Styles */
-    float: initial;
-    padding-right: initial;
-    margin-left: initial;
-    
-    /* Actual Styles */
-    position: absolute;
-    left: 0;
-    padding-right: 0.5rem;
-    transform: translateX(-100%);
-  }
   ${enumerateHeaderStyles(`.header-anchor svg`)} {
-    visibility: visible;
     opacity: 0;
+    visibility: visible;
     transition: opacity 0.25s linear;
-    width: 1.25rem;
-    height: 1.25rem;
   }
   ${enumerateHeaderStyles(`.header-anchor svg`, ":hover")},
   ${enumerateHeaderStyles(`.header-anchor:focus svg`)} {
