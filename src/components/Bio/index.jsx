a3b49bef9914eec8356cd82b3779229c6f91f036
Guy Thomas <guythomas@lyft.com>
2020-03-30 22:40:36 -0700

    Basic landing page

--- src/components/Bio/index.jsx
+++ src/components/Bio/index.jsx
@@ -6,44 +6,49 @@ const Avatar = styled.img`
   border-radius: 50%;
   width: 11rem;
   margin-bottom: 0;
+`
+
+const AvatarWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   grid-area: avatar;
 `
 
 const Header = styled.header`
   display: grid;
+  height: 100vh;
   grid-template-areas:
     "avatar avatar"
     "main   main";
 
   grid-template-columns: 30% 1fr;
+  grid-template-rows: min-content 1fr;
   @media (min-width: 1024px) {
     grid-template-areas:
       "avatar main"
       "avatar main";
   }
   width: 100%;
-  padding: 2rem 3.5rem 0.4rem;
+  padding-top: 2rem;
   max-width: 1276px;
 `
 
 const Description = styled.div`
   padding: 1.75rem 3.5rem 1.4rem;
-`
-
-const SocialIcons = styled.div`
-s`
-
-const FullName = styled.h1`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 0;
+  text-align: center;
   @media (min-width: 1024px) {
-    align-itmes: flex-start;
+    text-align: left;
   }
 `
 
-const Name = styled.span`
+const SocialIcons = styled.div``
+
+const FullName = styled.h1`
+  margin: 0;
+`
+
+const Name = styled.div`
   color: gray;
 `
 
@@ -56,6 +61,7 @@ const SecondName = styled(Name)`
 const Tagline = styled.h5`
   margin: 0;
   margin-top: 1rem;
+  text-align: inherit;
 `
 
 const ContactDetails = styled.div`
@@ -66,10 +72,16 @@ const ContactDetails = styled.div`
   }
   @media (min-width: 760px) {
     grid-template-columns: 1fr 1fr 1fr 1fr;
+    padding: 1.75rem 3.5rem 1.4rem;
+  }
+  @media (min-width: 1024px) {
+    bottom: 1rem;
+    position: absolute;
   }
 `
 
 const ContactDetail = styled.div`
+  padding-bottom: 1.75rem;
   @media (max-width: 760px) {
     display: flex;
     flex-direction: column;
@@ -85,11 +97,24 @@ const Divider = styled.div`
 
 const Main = styled.main`
   grid-area: main;
+  @media (min-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+  }
+`
+
+const ContactDetailHeader = styled.h4`
+  margin-top: 0;
+  margin-bottom: 0.5rem;
 `
 
 export const Bio = () => (
   <Header>
-    <Avatar src={avatarImage} />
+    <AvatarWrapper>
+      <Avatar src={avatarImage} />
+    </AvatarWrapper>
     <Main>
       <Description>
         <FullName>
@@ -101,21 +126,21 @@ export const Bio = () => (
       </Description>
       <ContactDetails>
         <ContactDetail>
-          <h4>Location</h4>
+          <ContactDetailHeader>Location</ContactDetailHeader>
           <div>San Francisco</div>
         </ContactDetail>
         <ContactDetail>
-          <h4>Phone</h4>
+          <ContactDetailHeader>Phone</ContactDetailHeader>
           <div>415 200 8333</div>
         </ContactDetail>
         <ContactDetail>
-          <h4>Web</h4>
+          <ContactDetailHeader>Web</ContactDetailHeader>
           <div>
             <a href="https://guythomas.me">guythomas.me</a>
           </div>
         </ContactDetail>
         <ContactDetail>
-          <h4>Email</h4>
+          <ContactDetailHeader>Email</ContactDetailHeader>
           <div>
             <a href="mailto:guythomas721@gmail.com">guythomas721@gmail.com</a>
           </div>
