5fa825c17f5c9d1e8a1d37f674318d86029e8854
Guy Thomas <guythomas@lyft.com>
2020-10-20 07:13:49 -0400

    make editale

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { css } from "@emotion/core"
 import styled from "@emotion/styled"
 import { COLOR_PALETTE } from "~styles"
 import { graphql } from "gatsby"
@@ -19,6 +20,7 @@ const DownloadSvg: React.FC<SVGProps> = ({ className }) => (
   <svg
     className={className}
     viewBox="0 0 8 11"
+    role="img"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
@@ -28,19 +30,36 @@ const DownloadSvg: React.FC<SVGProps> = ({ className }) => (
     <path d="M0 10H8V11H0V10Z" fill="inherit" />
   </svg>
 )
+const EditSvg: React.FC<SVGProps> = ({ className }) => (
+  <svg
+    className={className}
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 576 512"
+  >
+    <path d="M402.3 344.9l32-32c5-5 13.7-1.5 13.7 5.7V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h273.5c7.1 0 10.7 8.6 5.7 13.7l-32 32c-1.5 1.5-3.5 2.3-5.7 2.3H48v352h352V350.5c0-2.1.8-4.1 2.3-5.6zm156.6-201.8L296.3 405.7l-90.4 10c-26.2 2.9-48.5-19.2-45.6-45.6l10-90.4L432.9 17.1c22.9-22.9 59.9-22.9 82.7 0l43.2 43.2c22.9 22.9 22.9 60 .1 82.8zM460.1 174L402 115.9 216.2 301.8l-7.3 65.3 65.3-7.3L460.1 174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8 0L436 82l58.1 58.1 30.9-30.9c4-4.2 4-10.8-.1-14.9z"></path>
+  </svg>
+)
 
-const StyledSvg = styled(DownloadSvg)`
+const InteractiveSvgStyles = () => css`
   height: 1.5rem;
   width: 1.5rem;
   margin-left: 1rem;
   display: inline-block;
-  fill: ${() => COLOR_PALETTE.interactive.color};
+  fill: ${COLOR_PALETTE.interactive.color};
   &:hover {
-    fill: ${() => COLOR_PALETTE.interactiveActive.color};
+    fill: ${COLOR_PALETTE.interactiveActive.color};
   }
   @media print {
     display: none;
   }
+`
+
+const DowloadIcon = styled(DownloadSvg)`
+  ${InteractiveSvgStyles}
+`
+const EditIcon = styled(EditSvg)`
+  ${InteractiveSvgStyles}
 `
 
 const PageContainer = styled.div`
@@ -372,6 +391,7 @@ const Resume: React.FC<ResumeProps> = ({
   const { value: isDarkMode } = useDarkMode()
   const downloadLink = isDarkMode ? darkResumePdf : lightResumePdf
   const resumeData = nodes[0]
+  const [isEditing, setIsEditing] = React.useState(false);
   return (
     <ThemeProvider>
       <PageContainer>
@@ -386,17 +406,41 @@ const Resume: React.FC<ResumeProps> = ({
           <Bio>
             <Titles>
               <Names>
-                <FirstName>{resumeData.firstName}</FirstName>
-                <LastName>{resumeData.lastName}</LastName>
+                <FirstName contentEditable={isEditing}>
+                  {resumeData.firstName}
+                </FirstName>
+                <LastName contentEditable={isEditing}>
+                  {resumeData.lastName}
+                </LastName>
               </Names>
-              <Description>
+              <Description contentEditable={isEditing}>
                 {resumeData.tagline}
-                <a target="_blank" rel="noreferrer" href={downloadLink}>
-                  <StyledSvg />
+                <a
+                  style={{ cursor: "pointer" }}
+                  target="_blank"
+                  rel="noreferrer"
+                  onClick={isEditing ? window.print : undefined}
+                  href={isEditing ? undefined : downloadLink}
+                >
+                  <DowloadIcon />
+                </a>
+                <a
+                  style={{ cursor: "pointer" }}
+                  target="_blank"
+                  rel="noreferrer"
+                  onClick={() => {
+                    setIsEditing(!isEditing)
+                    requestAnimationFrame(() => {
+                      const firstHeader = document.querySelector("h1")
+                      if (firstHeader) firstHeader.focus()
+                    })
+                  }}
+                >
+                  <EditIcon />
                 </a>
               </Description>
             </Titles>
-            <Contacts>
+            <Contacts contentEditable={isEditing}>
               {resumeData.contactDetails?.map((contactDetail) => {
                 // Wow, TS really wants me to be safe here
                 const definitelyContactDetail = contactDetail || []
@@ -414,11 +458,13 @@ const Resume: React.FC<ResumeProps> = ({
         </BioWrapper>
         <IntroTitle>Intro</IntroTitle>
         <IntroContent>
-          <SectionContentInner>{resumeData.intro}</SectionContentInner>
+          <SectionContentInner contentEditable={isEditing}>
+            {resumeData.intro}
+          </SectionContentInner>
         </IntroContent>
         <ExperienceTitle>Experience</ExperienceTitle>
         <ExperienceSection>
-          <SectionContentInner>
+          <SectionContentInner contentEditable={isEditing}>
             {resumeData.experience?.map((experienceItem) => (
               <Timeline
                 key={String(experienceItem?.date)}
@@ -435,7 +481,7 @@ const Resume: React.FC<ResumeProps> = ({
         </ExperienceSection>
         <EducationTitle>Education</EducationTitle>
         <EducationSection>
-          <SectionContentInner>
+          <SectionContentInner contentEditable={isEditing}>
             {resumeData.education?.map((experienceItem) => (
               <Timeline
                 key={String(experienceItem?.date)}
