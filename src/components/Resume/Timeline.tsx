5ac6f4f21adc03f28d6276ae917ce8f1d7e70f16
Guy Thomas <guythomas@lyft.com>
2020-12-29 18:19:16 -0600

    add editable resumes

--- src/components/Resume/Timeline.tsx
+++ src/components/Resume/Timeline.tsx
@@ -2,22 +2,29 @@ import React from "react"
 import { Maybe } from "~types/gatsby-graphql"
 import styled from "@emotion/styled"
 import { DESKTOP } from "./constants"
+import { SectionButton } from "./styles"
 
 interface TimelineProps {
   date?: string
   company?: string
   title?: string
-  subtitle?: string
   detailItems?: Maybe<string>[]
   className?: string
+  contentEditable?: boolean
+  onRemove?: () => void
+  onChange?: (value: string) => void
+  innerHTML?: string
 }
 
 const TimelineOuter = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  @media ${DESKTOP} {
-    grid-gap: 2rem;
-    grid-template-columns: 1fr 2fr;
+  position: relative;
+  & > div {
+    display: grid;
+    grid-template-columns: 1fr;
+    @media ${DESKTOP} {
+      grid-gap: 2rem;
+      grid-template-columns: 1fr 2fr;
+    }
   }
 `
 
@@ -49,14 +56,6 @@ const TimelineTitle = styled.h4`
     margin-bottom: 0.5rem;
   }
 `
-const TimelineSubtitle = styled.span`
-  font-style: italic;
-  margin: 1rem 0 1rem;
-  @media print {
-    margin-top: 0.5rem;
-    margin-bottom: 0.5rem;
-  }
-`
 
 const TimelineListItem = styled.li`
   @media only print {
@@ -76,29 +75,79 @@ const TimelineList = styled.ul`
   }
 `
 
+const RemoveSection = styled(SectionButton)`
+  position: absolute;
+  right: 14px;
+  top: 30px;
+  @media ${DESKTOP} {
+    right: -50px;
+    top: 20px;
+  }
+`
 const Timeline: React.FC<TimelineProps> = ({
   date,
   company,
   title,
-  subtitle,
   detailItems = [],
   className = "",
-}) => (
-  <TimelineOuter className={className}>
-    <TimelineTitles>
-      {date && <TimelineDate>{date}</TimelineDate>}
-      {company && <TimelineCompany>{company}</TimelineCompany>}
-      {title && <TimelineTitle>{title}</TimelineTitle>}
-      {subtitle && <TimelineSubtitle>{subtitle}</TimelineSubtitle>}
-    </TimelineTitles>
-    <TimelineDetails>
-      <TimelineList>
-        {detailItems.map((bullet) => (
-          <TimelineListItem key={String(bullet)}>{bullet}</TimelineListItem>
-        ))}
-      </TimelineList>
-    </TimelineDetails>
-  </TimelineOuter>
-)
+  contentEditable = false,
+  onRemove,
+  onChange,
+  innerHTML,
+}) => {
+  const [initialHTML] = React.useState(innerHTML)
+  const test = React.useRef<HTMLDivElement>(null)
+  React.useEffect(() => {
+    if (contentEditable && onChange) {
+      onChange(test.current?.innerHTML || "")
+    }
+    // I don't want to force the memoization of onChange.
+    // Including in this array will cause an infinite loop since it changes on every render
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [contentEditable, test])
+
+  const content = initialHTML ? null : (
+    <>
+      <TimelineTitles>
+        {date && <TimelineDate>{date}</TimelineDate>}
+        {company && <TimelineCompany>{company}</TimelineCompany>}
+        {title && <TimelineTitle>{title}</TimelineTitle>}
+      </TimelineTitles>
+      <TimelineDetails>
+        <TimelineList>
+          {detailItems.map((bullet) => (
+            <TimelineListItem key={String(bullet)}>{bullet}</TimelineListItem>
+          ))}
+        </TimelineList>
+      </TimelineDetails>
+    </>
+  )
+
+  return (
+    <TimelineOuter className={className}>
+      {contentEditable && (
+        <RemoveSection onClick={onRemove} actionType="negative">
+          -
+        </RemoveSection>
+      )}
+      <div
+        ref={test}
+        contentEditable={contentEditable}
+        onInput={(event) => {
+          if (onChange) onChange(event.currentTarget.innerHTML)
+        }}
+        dangerouslySetInnerHTML={
+          initialHTML
+            ? {
+                __html: initialHTML,
+              }
+            : undefined
+        }
+      >
+        {content}
+      </div>
+    </TimelineOuter>
+  )
+}
 
 export default Timeline
