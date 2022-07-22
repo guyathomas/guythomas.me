3ba013d4e5f41eefc9ba3fe5272a3343737a2f97
Guy Thomas <guy.thomas@instacart.com>
2022-07-21 19:29:44 -0700

    remove unused components

--- packages/resume/src/styles/DarkMode.ts
+++ packages/resume/src/styles/DarkMode.ts
@@ -1,33 +1,33 @@
-const DM_MEDIA_QUERY = "(prefers-color-scheme: dark)"
+const DM_MEDIA_QUERY = "(prefers-color-scheme: dark)";
 
-type MQCallback = (event: MediaQueryListEvent) => void
-type DarkModeListener = (darkMode: boolean) => void
-type Unobserve = () => void
+type MQCallback = (event: MediaQueryListEvent) => void;
+type DarkModeListener = (darkMode: boolean) => void;
+type Unobserve = () => void;
 
 class DarkMode {
   private get mediaQuery(): MediaQueryList | undefined {
-    return global.matchMedia ? global.matchMedia(DM_MEDIA_QUERY) : undefined
+    return global.matchMedia ? global.matchMedia(DM_MEDIA_QUERY) : undefined;
   }
   public get enabled(): boolean {
     if (typeof document === "undefined") {
-      return !this.mediaQuery?.matches
+      return !this.mediaQuery?.matches;
     }
-    const classList = document.body.classList || []
+    const classList = document.body.classList;
     if (!classList.contains("dark-mode") && !classList.contains("light-mode")) {
-      return !this.mediaQuery?.matches
+      return !this.mediaQuery?.matches;
     }
-    return classList.contains("dark-mode")
+    return classList.contains("dark-mode");
   }
 
   public observe(callback: DarkModeListener): Unobserve {
     const changeListener: MQCallback = (event) => {
-      callback(event.matches)
-    }
-    this.mediaQuery?.addEventListener("change", changeListener)
+      callback(event.matches);
+    };
+    this.mediaQuery?.addEventListener("change", changeListener);
     return () => {
-      this.mediaQuery?.removeEventListener("change", changeListener)
-    }
+      this.mediaQuery?.removeEventListener("change", changeListener);
+    };
   }
 }
 
-export const darkMode = new DarkMode()
+export const darkMode = new DarkMode();
