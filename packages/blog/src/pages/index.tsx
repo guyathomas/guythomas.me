bbfc99b80494aa12143e3c418aa264c160bd1843
Guy Thomas <guy.thomas@instacart.com>
2022-09-10 17:44:34 -0700

    add layout

--- packages/blog/src/pages/index.tsx
+++ packages/blog/src/pages/index.tsx
@@ -4,6 +4,7 @@ import cx from "classnames";
 import { GetStaticProps } from "next";
 import { getSortedPostsData, PostMeta } from "~/lib/posts";
 import PostSummary from "~/components/PostSummary";
+import styles from "./index.module.css";
 
 const POST_COUNT = 5;
 interface HomeProps {
@@ -46,50 +47,57 @@ const Home: React.FC<HomeProps> = ({ allPostsData }) => {
 
   return (
     <section className="prose m-auto">
-      <div className="bx-container font-normal">
-        <h3 className="text-rose-600 uppercase font-normal">
-          Recently Published
-        </h3>
-        <div className="pl-0 flex">
-          {tagsByFrequency.map((tag) => (
-            <button
-              key={tag}
-              className={cx(
-                tagFilters.has(tag) ? "bg-cyan-500" : "bg-cyan-800",
-                "pl-2",
-                "pr-2",
-                "text-white",
-                "list-none",
-                "pl-0",
-                "p-1",
-                "rounded",
-                "leading-5",
-                "mr-2"
-              )}
-              onClick={() => {
-                const clonedFilters = Array.from(tagFilters);
-                const newTagArray = tagFilters.has(tag)
-                  ? clonedFilters.filter((existingTag) => existingTag !== tag)
-                  : [...clonedFilters, tag];
-                setTagFilters(new Set(newTagArray));
-              }}
-            >
-              {tag}
-            </button>
-          ))}
+      <div className={cx("bx-container", "font-normal", styles.layout)}>
+        <div>
+          <h3 className="text-rose-600 uppercase font-normal">
+            Recently Published
+          </h3>
+
+          <ul className="pl-0">
+            {filteredPostData.map(({ id, date, title, description }) => (
+              <li key={id} className="list-none pl-0">
+                <PostSummary
+                  id={id}
+                  title={title}
+                  description={description}
+                  date={date}
+                />
+              </li>
+            ))}
+          </ul>
         </div>
-        <ul className="pl-0">
-          {filteredPostData.map(({ id, date, title, description }) => (
-            <li key={id} className="list-none pl-0">
-              <PostSummary
-                id={id}
-                title={title}
-                description={description}
-                date={date}
-              />
-            </li>
-          ))}
-        </ul>
+        <div>
+          <h3 className="text-rose-600 uppercase font-normal">Tags</h3>
+          <div className="pl-0 flex">
+            {tagsByFrequency.map((tag) => (
+              <button
+                key={tag}
+                className={cx(
+                  tagFilters.has(tag) ? "bg-cyan-500" : "bg-cyan-800",
+                  "pl-2",
+                  "pr-2",
+                  "text-white",
+                  "list-none",
+                  "pl-0",
+                  "p-1",
+                  "rounded-md",
+                  "leading-5",
+                  "mr-2",
+                  "flex-shrink-0"
+                )}
+                onClick={() => {
+                  const clonedFilters = Array.from(tagFilters);
+                  const newTagArray = tagFilters.has(tag)
+                    ? clonedFilters.filter((existingTag) => existingTag !== tag)
+                    : [...clonedFilters, tag];
+                  setTagFilters(new Set(newTagArray));
+                }}
+              >
+                {tag}
+              </button>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
